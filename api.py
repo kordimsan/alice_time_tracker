cc53e96143f6ec74e1044a6ff827d7d66a980fb3
Dmitry Korzunin <kordimsan@mail.ru>
2021-11-28 00:20:11 +0500

    add response_model

--- api.py
+++ api.py
@@ -4,13 +4,14 @@ from fastapi import Body, FastAPI
 
 from app import handle_dialog
 from models.request import AliceRequest
+from models.response import AliceResponse
 
 app = FastAPI()
 
 logging.basicConfig(level=logging.DEBUG)
 
 
-@app.post("/")
+@app.post("/", response_model=AliceResponse)
 async def main(request: AliceRequest = Body(...)):  # noqa
     logging.info("Request: %r", request)
     response = handle_dialog(request)
