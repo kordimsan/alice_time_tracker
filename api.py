e26b1a6391372f1a76c3547f857ce2bdababfac7
Dmitry Korzunin <kordimsan@mail.ru>
2021-11-27 23:49:05 +0500

    add pydantic models

--- api.py
+++ api.py
@@ -1,25 +1,21 @@
 import logging
 
 from fastapi import Body, FastAPI
-from pydantic import BaseModel
 
 from app import handle_dialog
+from models.request import AliceRequest
 
 app = FastAPI()
 
 logging.basicConfig(level=logging.DEBUG)
 
 
-class Request(BaseModel):
-    request = Body(...)
-
-
 # Задаем параметры приложения FastAPI.
 @app.post("/")
-async def main(request=Request):
+async def main(request: AliceRequest = Body(...)):  # noqa
     # Функция получает тело запроса и возвращает ответ.
     logging.info("Request: %r", request)
-
+    request = request.dict()
     response = {
         "version": request["version"],
         "session": request["session"],
