209176175d89752854a06b631ab6471359707f1d
Dmitry Korzunin <kordimsan@mail.ru>
2021-12-06 11:57:46 +0500

    del logtail

--- api.py
+++ api.py
@@ -1,8 +1,6 @@
 import logging
-import os
 
 from fastapi import Body, FastAPI
-from logtail import LogtailHandler
 
 from app import handle_dialog
 from models.request import AliceRequest
@@ -10,11 +8,7 @@ from models.response import AliceResponse
 
 app = FastAPI()
 
-handler = LogtailHandler(source_token=os.environ.get("LOGTAIL_SOURCE_TOKEN"))
-logger = logging.getLogger(__name__)
-logger.handlers = []
-logger.setLevel(logging.DEBUG)
-logger.addHandler(handler)
+logging.setLevel(logging.DEBUG)
 
 
 @app.get("/")
@@ -24,7 +18,7 @@ async def check_api():
 
 @app.post("/", response_model=AliceResponse)
 async def main(request: AliceRequest = Body(...)):  # noqa
-    logger.info("Request: %r", request)
+    logging.info("Request: %r", request)
 
     response, tasks = handle_dialog(request)
 
